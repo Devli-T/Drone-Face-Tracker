81b14a51ab8a1b20a9a6fc22b8e8895fe3b058d4
AnUnsocialPigeon <jdturrell@gmail.com>
2024-11-02 01:38:26 +0000

    Test with a basic visual?

--- batteryChecker.py
+++ batteryChecker.py
@@ -3,7 +3,7 @@ import tello as drone
 
 drone.start()
 power = drone.get_battery()
-print("Power Level: ", power, "%")
+print(f"Power Level: {power}%")
 drone.takeoff()
 drone.land()
 
--- main.py
+++ main.py
@@ -1,8 +1,47 @@
-# Test main file?
+import tello as drone
+import cv2
+import time
+import threading
 
-print("Hello, world!")
+stop_video_feed = False
+def show_drone_video_feed():
+    global stop_video_feed
+    while True:
+        frame = drone.get_video_frame()
+
+        if frame is None:
+            print("No video feed available.")
+            break
+
+        # Display the frame in a window
+        cv2.imshow('Drone Video Feed', frame)
+
+        # Exit the video feed window if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+
+print("Starting drone...")
+drone.start()
+
+print("Beginning video stream...")
+video_thread = threading.Thread(target=show_drone_video_feed)
+video_thread.start()
+
+print("Taking off...")
+drone.takeoff()
+
+time.sleep(5)
+
+print("Landing...")
+drone.land()
+
+print("Quitting...")
+stop_video_feed = True
+video_thread.join()
 
 
-
+print("Done.")
 
 
